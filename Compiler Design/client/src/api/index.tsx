1b7d80674cebac3f819375741cdb27a4f4907bfd
Pranjal Sailwal <127615786+sailwalpranjal@users.noreply.github.com>
2025-05-12 08:35:17 +0000

    testing backend

--- Compiler Design/client/src/api/index.tsx
+++ Compiler Design/client/src/api/index.tsx
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios"
 
-const pistonBaseUrl = "https://emkc.org/api/v2/piston"
+const pistonBaseUrl = "https://pbl-project-compiler-design-sever.onrender.com/api/piston"
 
 const instance: AxiosInstance = axios.create({
     baseURL: pistonBaseUrl,
